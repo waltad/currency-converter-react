3f4b9af8c969904944c600e95e0b34788c0cfc84
GI10 <tadus@op.pl>
2023-04-17 13:11:52 +0200

    set background to whole page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import Form from "./Form";
 import ResultBox from "./ResultBox";
 import Container from "./Container";
-import Background from "./Background";
 
 function App() {
   return (
-    <Background>
-      <Container>
-        <Form />
-        <ResultBox />
-      </Container>
-    </Background>
+    <Container>
+      <Form />
+      <ResultBox />
+    </Container>
   );
 }
 
--- src/Background/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css"
-
-const Background = (props) => (
-  <section className="background">
-    {props.children}
-  </section>
-);
-
-export default Background;
