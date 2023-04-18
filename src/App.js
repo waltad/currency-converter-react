331b06dad10a14c48c019f404f7f7f564f6d0b28
Tadek Waluga <tadek.waluga@gmail.com>
2023-04-18 21:45:31 +0200

    added exchange rate table calulateResult

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Form from "./Form";
 import ResultBox from "./ResultBox";
 import Container from "./Container";
+import { useState } from "react";
 
 function App() {
+  const [rate, setRate] = useState([
+    {key: "EUR", rate: 4.6286},
+    {key: "USD", rate: 4.2151},
+    {key: "GBP", rate: 5.2428},
+  ]);
+  const calculateResult = (newAmount) => {
+
+  };
+  
   return (
     <Container>
-      <Form />
+      <Form
+        calculateResult={calculateResult}
+      />
       <ResultBox />
     </Container>
   );
