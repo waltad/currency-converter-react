39daa6fd6e756a2278d7f2a101519adcb443e115
GI10 <tadus@op.pl>
2023-05-11 13:48:24 +0200

    rounded the result to 2 decimal places

--- src/ResultBox/index.js
+++ src/ResultBox/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 
 const ResultBox = ({ newValue, result, currencyRate }) => (
   <p className="resultBox">
-    Za <span>{newValue ? newValue : 0}</span> PLN kupisz <span>{result ? result : "N/A"} {currencyRate[0].name}</span>
+    Za <span>{newValue ? newValue : 0}</span> PLN kupisz <span>{result ? result.toFixed(2) : "N/A"} {currencyRate[0].name}</span>
   </p>
 );
 
