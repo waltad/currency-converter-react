e33639a921f652d9b20a42d470eb559d376c48f9
Tadek Waluga <tadek.waluga@gmail.com>
2023-05-14 17:47:48 +0200

    correcting code formatting

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import { useState } from "react";
 
 function App() {
   const currencyTable = [
-    {name: "EUR", rate: 4.6286},
-    {name: "USD", rate: 4.2151},
-    {name: "GBP", rate: 5.2428},
+    { name: "EUR", rate: 4.6286 },
+    { name: "USD", rate: 4.2151 },
+    { name: "GBP", rate: 5.2428 },
   ];
   const [result, setResult] = useState({});
 
   const calculateResult = (amount, name) => {
     const rate = currencyTable.find(value => value.name === name).rate;
-    setResult({value: +amount, outcome: +amount / rate, currency: name});
+    setResult({
+      value: +amount,
+      outcome: +amount / rate,
+      currency: name
+    });
   };
-  
+
   return (
     <Container>
       <Form
--- src/ResultBox/index.js
+++ src/ResultBox/index.js
@@ -2,7 +2,8 @@ import "./style.css";
 
 const ResultBox = ({ result }) => (
   <p className="resultBox">
-    Za <span>{result.value ? (result.value).toFixed(2) : 0}</span> PLN kupisz <span>{result.outcome ?(result.outcome).toFixed(2) : "N/A"} {result.currency}</span>
+    Za <span>{result.value ? (result.value).toFixed(2) : 0}</span> PLN kupisz
+    <span> {result.outcome ? (result.outcome).toFixed(2) : "N/A"} {result.currency}</span>
   </p>
 );
 
