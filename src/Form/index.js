1ec9e46aa3ae71d00f79d0645b0da20231dde88a
Tadek Waluga <tadek.waluga@gmail.com>
2023-05-14 18:28:31 +0200

    added rendering in currency selection

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,57 +1,63 @@
 import { useState } from "react";
 import "./style.css";
+import { currencyTable } from "../currencytable";
 
 const Form = ({ calculateResult }) => {
-    const [newValue, setNewValue] = useState("");
-    const [currency, setCurrency] = useState("EUR");
+  const [newValue, setNewValue] = useState("");
+  const [currency, setCurrency] = useState("EUR");
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(newValue, currency);
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(newValue, currency);
+  };
 
-    return (
-        <form onSubmit={onFormSubmit} className="form">
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">Przelicznie wartości EUR</legend>
-                <p>
-                    <label>
-                        <span className="form__labelText">
-                            Podaj wartość w PLN:
-                        </span>
-                        <input
-                            value={newValue}
-                            onChange={({ target }) => setNewValue(target.value)}
-                            className="form__field"
-                            required
-                            type="number" min="0" max="1000000" step="0.01"
-                        />
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        <span className="form__labelText">
-                            Wybierz walutę:
-                        </span>
-                        <select
-                            value={currency}
-                            onChange={({ target }) => setCurrency(target.value)}
-                            className="form__field"
-                        >
-                            <option value="EUR">EUR</option>
-                            <option value="USD">USD</option>
-                            <option value="GBP">GBP</option>
-                        </select>
-                    </label>
-                </p>
-            </fieldset>
-            <p>
-                <button className="form__button">
-                    Zawierdź
-                </button>
-            </p>
-        </form>
-    )
+  return (
+    <form onSubmit={onFormSubmit} className="form">
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Przelicznie wartości EUR</legend>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Podaj wartość w PLN:
+            </span>
+            <input
+              value={newValue}
+              onChange={({ target }) => setNewValue(target.value)}
+              className="form__field"
+              required
+              type="number" min="0" max="1000000" step="0.01"
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText">
+              Wybierz walutę:
+            </span>
+            <select
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+              className="form__field"
+            >
+              {currencyTable.map((currency => (
+                <option
+                  key={currency.name}
+                  value={currency.name}
+                >
+                  {currency.name}
+                </option>
+              )))}
+            </select>
+          </label>
+        </p>
+      </fieldset>
+      <p>
+        <button className="form__button">
+          Zawierdź
+        </button>
+      </p>
+    </form>
+  )
 };
 
 export default Form;
