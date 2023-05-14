ee63807636c7c6fe0b3bed6804fdada740d9e122
Tadek Waluga <tadek.waluga@gmail.com>
2023-05-14 18:05:23 +0200

    moving the currency table to the file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Form from "./Form";
 import ResultBox from "./ResultBox";
 import Container from "./Container";
+import { currencyTable } from "./currencytable";
 import { useState } from "react";
 
 function App() {
-  const currencyTable = [
-    { name: "EUR", rate: 4.6286 },
-    { name: "USD", rate: 4.2151 },
-    { name: "GBP", rate: 5.2428 },
-  ];
   const [result, setResult] = useState({});
 
   const calculateResult = (amount, name) => {
---
+++ src/currencytable.js
@@ -0,0 +1,5 @@
+export const currencyTable = [
+  { name: "EUR", rate: 4.6286 },
+  { name: "USD", rate: 4.2151 },
+  { name: "GBP", rate: 5.2428 },
+];
