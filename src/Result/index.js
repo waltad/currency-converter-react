46c98112c292d4f743083507a8b359d2bb354a42
Tadek Waluga <tadek.waluga@gmail.com>
2023-05-31 20:47:30 +0200

    renamed resultBox className to result

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Result = ({ result }) => (
-  <p className="resultBox">
+  <p className="result">
     Za <span>{result.value ? (result.value).toFixed(2) : 0}</span> PLN kupisz
     <span> {result.outcome ? (result.outcome).toFixed(2) : "N/A"} {result.currency}</span>
   </p>
