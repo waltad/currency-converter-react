70d45484e39320f415fc71103048fa1513a21025
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-06-12 14:50:31 +0200

    changing useExchangeData

--- src/useExchangeData.js
+++ src/useExchangeData.js
@@ -1,23 +1,35 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const [currenciesTable, setCurrenciesTable] = useState({
-  state: "loading",
-});
+
 
 const apiUrl = "https://api.exchangerate.host/latest?base=PLN";
 
 export const useExchangeData = () => {
-  (async () => {
-    try {
-      const response = await axios.get(apiUrl);
-      setCurrenciesTable({
-        state: "success",
-        date: response.data.date,
-        rates: response.data.rates,
-      });
-    } catch (error) {
-      console.error("Something bad happened!", error);
-    }
-  })();
+  const [currenciesTable, setCurrenciesTable] = useState({
+    state: "loading"
+  });
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+        
+        setCurrenciesTable({
+          state: "success",
+          date: data.date,
+          rates: data.rates
+        });
+      } catch (error) {
+        setCurrenciesTable({
+          state: `${error}`
+        });
+      }
+    })();
+
+    setTimeout(2000);
+  }, [currenciesTable]);
+
+  return currenciesTable;
 };
