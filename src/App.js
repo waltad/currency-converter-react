73d320e4687bc1489a76dedafc5a701a35b4e229
Tadeusz Waluga <tadek.waluga@gmail.com>
2023-06-16 13:02:26 +0200

    added exchancheRatesData and curresiesTable

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Form from "./Container/Form";
 import Result from "./Container/Result";
 import Container from "./Container";
-import { currencyTable } from "./currencytable";
 import { useState } from "react";
+import { useExchangeRatesData } from "./useExchangeRatesData";
 
 function App() {
   const [result, setResult] = useState({});
+  const exchangeRatesData = useExchangeRatesData();
+  const currenciesTable = exchangeRatesData.rates;
 
   const calculateResult = (amount, name) => {
-    const rate = currencyTable.find(value => value.name === name).rate;
+    const rate = currenciesTable.find(value => value.name === name).rate;
     setResult({
       value: +amount,
-      outcome: +amount / rate,
+      outcome: +amount * rate,
       currency: name
     });
   };
@@ -20,6 +22,8 @@ function App() {
     <Container>
       <Form
         calculateResult={calculateResult}
+        exchangeRatesData={exchangeRatesData}
+        currenciesTable={currenciesTable}
       />
       <Result
         result={result}
