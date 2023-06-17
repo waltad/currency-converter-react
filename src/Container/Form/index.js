d500fa5ebf7542d3e0acb8a34bbca847ba4749b1
Tadek Waluga <tadek.waluga@gmail.com>
2023-06-17 17:47:07 +0200

    added error handling and info about rates

--- src/Container/Form/index.js
+++ src/Container/Form/index.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { Fieldset, Legend, TextLabel, Input, Button, Loading } from "./styled";
+import { Fieldset, Legend, TextLabel, Input, Button, Loading, Error, InfoAboutRates } from "./styled";
 import Clock from "./Clock";
 
-export const Form = ({ calculateResult, exchangeRatesData, currenciesTable}) => {
+export const Form = ({ calculateResult, exchangeRatesData, currenciesTable }) => {
   const [newValue, setNewValue] = useState("");
   const [currency, setCurrency] = useState("EUR");
 
@@ -14,50 +14,60 @@ export const Form = ({ calculateResult, exchangeRatesData, currenciesTable}) => 
   return (
     <form onSubmit={onFormSubmit}>
       {exchangeRatesData.state === "loading" ?
-      (
-        <Loading>Ładuję tebelę kursów</Loading>
-      ) :
-      (
-        <>
-          <Fieldset>
-            <Legend>Przelicznik walut</Legend>
-            <Clock />
+        (
+          <Loading>Ładuję tebelę kursów</Loading>
+        ) :
+        exchangeRatesData.state === "error" ? (
+          <Error>
+            Wystąpił błąd: "{exchangeRatesData.info}".<br />
+            Sprawdź połączenie z internetem
+          </Error>
+        ) :
+        (
+          <>
+            <Fieldset>
+              <Legend>Przelicznik walut</Legend>
+              <Clock />
+              <p>
+                <label>
+                  <TextLabel>Podaj wartość w PLN:</TextLabel>
+                  <Input
+                    value={newValue}
+                    onChange={({ target }) => setNewValue(target.value)}
+                    required
+                    type="number"
+                    min="0"
+                    max="1000000"
+                    step="0.01"
+                  />
+                </label>
+              </p>
+              <p>
+                <label>
+                  <TextLabel>Wybierz walutę:</TextLabel>
+                  <Input
+                    as="select"
+                    value={currency}
+                    onChange={({ target }) => setCurrency(target.value)}
+                  >
+                    {currenciesTable.map((valueDate) => (
+                      <option key={valueDate.name} value={valueDate.name}>
+                        {valueDate.name}
+                      </option>
+                    ))}
+                  </Input>
+                </label>
+              </p>
+              <InfoAboutRates>
+                Kursy pobrano ze strony: https://exchangerate.host<br />
+                Aktualne na dzień: <strong>{exchangeRatesData.date}</strong>
+              </InfoAboutRates>
+            </Fieldset>
             <p>
-              <label>
-                <TextLabel>Podaj wartość w PLN:</TextLabel>
-                <Input
-                  value={newValue}
-                  onChange={({ target }) => setNewValue(target.value)}
-                  required
-                  type="number"
-                  min="0"
-                  max="1000000"
-                  step="0.01"
-                />
-              </label>
+              <Button>Zawierdź</Button>
             </p>
-            <p>
-              <label>
-                <TextLabel>Wybierz walutę:</TextLabel>
-                <Input
-                  as="select"
-                  value={currency}
-                  onChange={({ target }) => setCurrency(target.value)}
-                >
-                  {currenciesTable.map((valueDate) => (
-                    <option key={valueDate.name} value={valueDate.name}>
-                      {valueDate.name}
-                    </option>
-                  ))}
-                </Input>
-              </label>
-            </p>
-          </Fieldset>
-          <p>
-            <Button>Zawierdź</Button>
-          </p>
-        </>
-      )}
+          </>
+        )}
     </form>
   );
 };
