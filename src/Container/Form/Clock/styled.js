fe372fcd8b8cd75bb21cc91716b1c0235f01adf5
Tadek Waluga <tadek.waluga@gmail.com>
2023-06-18 18:34:25 +0200

    changed pading-left

--- src/Container/Form/Clock/styled.js
+++ src/Container/Form/Clock/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledClock = styled.p`
   margin: 0 0 30px 0;
-  padding-left: 30%;
+  padding-left: 25%;
   font-family:'Courier New', Courier, monospace;
   font-size: small;
 `;
